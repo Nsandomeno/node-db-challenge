fa5f0e7935feb69b59120fee6b12c04200b91271
Nicholas Sandomeno <nicholassandomeno@Nicholass-Air.home>
2020-02-21 13:23:34 -0500

    MVP complete.

--- api/project-model.js
+++ api/project-model.js
@@ -3,10 +3,38 @@ const db = require('../data/dbConfig.js')
 // export all the functions as you build them
 module.exports = {
     // helper functions:
-    getProjects
+    getProjects,
+    addProject,
+    getResource,
+    addResource,
+    getTasks,
+    addTask
 }
 
 // Begin creating methods
 function getProjects() {
     return db('projects')
+}
+
+function addProject(project){
+    return db('projects')
+        .insert(project, "id")
+}
+
+function getResource() {
+    return db('resources')
+}
+
+function addResource(resource) {
+    return db('resources')
+            .insert(resource, "id")
+}
+
+function getTasks() {
+    return db('tasks')
+}
+
+function addTask(task) {
+    return db('tasks')
+        .insert(task, 'id')
 }
--- api/project-router.js
+++ api/project-router.js
@@ -15,5 +15,70 @@ router.get('/', (req, res) => {
         })
 })
 
+router.post('/', (req, res) => {
+    const project = req.body
+    if (!project.name) {
+        res.status(400).json({ message:"Please provide a name." })
+    } else {
+        Projects.addProject(project)
+        .then((id) => {
+            res.status(201).json(id)
+        })
+        .catch((error) => {
+            res.status(500).json({ message:"The promise could not be resolved." })
+        })
+    }
+})
+
+router.get('/resources', (req, res) => {
+    Projects.getResource()
+        .then((resources) => {
+            res.status(200).json(resources)
+        })
+        .catch((error) => {
+            res.status(500).json({ message:"The promise could not be resolved." })
+        })
+})
+
+router.post('/resources', (req, res) => {
+    const resource = req.body
+    if(!resource.name) {
+        res.status(400).json({ message:"Please provide a name." })
+    } else {
+        Projects.addResource(resource)
+            .then((id) => {
+                res.status(201).json(id)
+            })
+            .catch((error) => {
+                res.status(500).json({message:"The promise could not be resolved."})
+            })
+    }
+})
+
+router.get('/tasks', (req, res) => {
+    Projects.getTasks()
+        .then((tasks) => {
+            res.status(200).json(tasks)
+        })
+        .catch((error) => {
+            res.status(500).json({ message:"The promise could not be resolved." })
+        })
+})
+
+router.post('/tasks', (req, res) => {
+    const task = req.body
+    if (!task.description || !task.project_id) {
+        res.status(400).json({message:"Please include a description and a project_id"})
+    } else {
+        Projects.addTask(task)
+            .then((id) => {
+                res.status(201).json(id)
+            })
+            .catch((error) => {
+                res.status(500).json({message:"The promise could not be resolved."})
+            })
+    }
+})
+
 // export to server
 module.exports = router
